39df1f74daee9c194d5cadb6c2e5529deb7d4f02
yanoshi <googreimu@gmail.com>
2015-09-12 18:56:52 +0900

    PlatformIDが取得できたぞい

--- opencl-with-csharp-4test/Program.cs
+++ opencl-with-csharp-4test/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace opencl_with_csharp_4test
 {
+
     class Program
     {
+        public const string OpenCL = "OpenCL.dll";
+
+        //対象のDLLを読み込んで、DLL中のメソッドを実行可能にします。
+        //ここではDLL中の'clGetPlatformIDs'メソッドを利用します。
+        [DllImport(OpenCL)]
+        public static extern int clGetPlatformIDs
+            (uint num_entries,
+            IntPtr[] platforms,
+            out uint num_platforms);
+
+        public const int CL_SUCCESS = 0;
+
         static void Main(string[] args)
         {
+
+            uint platformCount;
+
+            int errcode = clGetPlatformIDs(0u, null, out platformCount);
+
+            if (errcode != CL_SUCCESS)
+                Console.WriteLine("Error at clGetPlatformIDs : " + errcode);
+            else
+                Console.WriteLine("Number of OpenCL Platforms : " + platformCount.ToString());
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ opencl-with-csharp-4test/set_env.bat
@@ -0,0 +1,3 @@
+﻿setx /M CUDA_INC_PATH "%CUDA_PATH%\include"
+setx /M CUDA_BIN_PATH "%CUDA_PATH%\bin"
+setx /M CUDA_LIB_PATH "%CUDA_PATH%\lib"
