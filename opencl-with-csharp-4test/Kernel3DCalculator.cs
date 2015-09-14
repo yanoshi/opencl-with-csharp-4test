36493086b1f1b91641630ee4f971ac5df53d3d5a
yanoshi <googreimu@gmail.com>
2015-09-15 03:55:13 +0900

    GridSizeが大きすぎるバグあり

--- opencl-with-csharp-4test/Kernel3DCalculator.cs
+++ opencl-with-csharp-4test/Kernel3DCalculator.cs
@@ -13,9 +13,9 @@ namespace opencl_with_csharp_4test
 {
     public class Kernel3DCalculator
     {
-        public const int SIZEX = 12;
-        public const int SIZEY = 12;
-        public const int SIZEZ = 12;
+        public const int SIZEX = 128;
+        public const int SIZEY = 128;
+        public const int SIZEZ = 128;
 
         public const int KERNEL_SIZEX = 3;
         public const int KERNEL_SIZEY = 3;
@@ -40,12 +40,11 @@ namespace opencl_with_csharp_4test
             ushort[,,] hostSource = new ushort[SIZEX,SIZEY,SIZEZ];
             float[,,] hostKernel = new float[KERNEL_SIZEX, KERNEL_SIZEY, KERNEL_SIZEZ];
             ushort[,][] output = new ushort[SIZEY,SIZEZ][];
-
-            ushort[] dev_outputtemp = gpu.Allocate<ushort>(SIZEX);
+            
             for (int z = 0; z < hostSource.GetLength(2); z++)
                 for (int y = 0; y < hostSource.GetLength(1); y++)
                     for (int x = 0; x < hostSource.GetLength(0); x++)
-                        hostSource[x, y, z] = (ushort)((z * SIZEX * SIZEY + y * SIZEX + x) % 128);
+                        hostSource[x, y, z] = (ushort)((z * SIZEX * SIZEY + y * SIZEX + x) % KERNEL_SIZEX);
 
             for (int z = 0; z < hostKernel.GetLength(2); z++)
                 for (int y = 0; y < hostKernel.GetLength(1); y++)
@@ -61,17 +60,49 @@ namespace opencl_with_csharp_4test
             for(int z = 0;z<SIZEZ;z++)
                 for(int y = 0;y<SIZEY;y++)
                 {
-                    ushort[] outputTemp = new ushort[SIZEX];
-
-                    gpu.Launch(SIZEZ*SIZEY, 1).ApplyKernel(z,dev_outputtemp);
+                    output[y, z] = new ushort[SIZEX];
+                    //何等分に分けるか
+                    int blockCount = SIZEX/12;
+                    if (blockCount == 0)
+                        blockCount = 1;
+                    int blockSize = SIZEX / blockCount;
                     
-                    gpu.CopyFromDevice(dev_outputtemp, outputTemp);
 
-                    output[y, z] = outputTemp;
+                    for (int i = 0; i < blockCount; i++)
+                    {
+                        if (i == blockCount - 1)
+                            blockSize += SIZEX % blockCount;
+
+                        ushort[] outputTemp = new ushort[blockSize];
+                        ushort[] dev_outputtemp = gpu.Allocate<ushort>(blockSize);
+
+                        if (i == blockCount - 1)
+                        {
+                            outputTemp[0] = (ushort)(blockSize * i);
+                            outputTemp[1] = SIZEX;
+                            gpu.CopyToDevice(outputTemp, dev_outputtemp);
+                            gpu.Launch(SIZEZ * SIZEY, 1).ApplyKernel(z, dev_outputtemp);
+                        }
+                        else
+                        {
+
+                            outputTemp[0] = (ushort)(blockSize * i);
+                            outputTemp[1] = (ushort)(blockSize * (i + 1));
+                            gpu.CopyToDevice(outputTemp, dev_outputtemp);
+                            gpu.Launch(SIZEZ * SIZEY, 1).ApplyKernel(z, dev_outputtemp);
+                        }
+
+                        gpu.CopyFromDevice(dev_outputtemp, outputTemp);
+
+
+                        Array.Copy(outputTemp, output[y, z], blockSize * i);
+
+
+                        gpu.Free(dev_outputtemp);
+                    }
+                   
                 }
             
-
-            gpu.Free(dev_outputtemp);
             Console.WriteLine("Success!!!!!");
         }
 
@@ -82,11 +113,12 @@ namespace opencl_with_csharp_4test
         [Cudafy]
         public static void ApplyKernel(GThread thread, int z, ushort[] outputData)
         {
-            int x = 0;
+            int x = outputData[0];
             int y = thread.blockIdx.x % SIZEY;
             float value = 0;
+            int endX = outputData[1];
 
-            while(x < SIZEX)
+            while (x < endX)
             {
                 for(int kernelZ = KERNEL_SIZEZ / -2; kernelZ <= KERNEL_SIZEZ /2; kernelZ++)
                     for (int kernelY = KERNEL_SIZEY / -2; kernelY <= KERNEL_SIZEY / 2; kernelY++)
